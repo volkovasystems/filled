081f7260dc6c7b24f31faef787a506651032667e
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-04 15:53:20 +0800

    Updated server-side test.

--- filled-test.js
+++ filled-test.js
@@ -3,7 +3,9 @@ const assert = require( "assert" );
 const filled = require( "./filled.js" );
 
 assert.equal( filled( [ 1, 2, 3 ] ), true, "should be true" );
+
 assert.equal( filled( [ { } ] ), true, "should be true" );
+
 assert.equal( filled( [ ] ), false, "should be false" );
 
 console.log( "ok" );
