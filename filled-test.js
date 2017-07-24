6abbfac61261d96046ea574ca8369630b3c2f79c
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-24 16:21:48 +0800

    Updated test.

--- filled-test.js
+++ filled-test.js
@@ -2,10 +2,10 @@
 const assert = require( "assert" );
 const filled = require( "./filled.js" );
 
-assert.equal( filled( [ 1, 2, 3 ] ), true, "should be true" );
+assert.equal( filled( [ 1, 2, 3 ] ), true, "should return true" );
 
-assert.equal( filled( [ { } ] ), true, "should be true" );
+assert.equal( filled( [ { } ] ), true, "should return true" );
 
-assert.equal( filled( [ ] ), false, "should be false" );
+assert.equal( filled( [ ] ), false, "should return false" );
 
 console.log( "ok" );
